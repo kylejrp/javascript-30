d6cf727d3d8bfa337f7ea624a8d97bebc1c5e86d
Kyle Pollard <kylejrp@gmail.com>
2020-06-22 11:32:06 -0700

    Basic Rock Band 4 note hitter design

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ document.addEventListener('keypress', hitDrum);
 
 function hitDrum(e) {
     let dataAttribute = `[data-key="${e.key.toLowerCase()}"]`
-    let drumElement = document.querySelector(`.key${dataAttribute}`);
+    let drumElements = document.querySelectorAll(`.key${dataAttribute}, .frame${dataAttribute}`);
     let audioElement = document.querySelector(`audio${dataAttribute}`)
 
-    if (drumElement) {
-        // Toggle the visual styles
-        drumElement.classList.add('playing');
-        setTimeout(() => drumElement.classList.remove('playing'), 50)
+    if (drumElements) {
+        drumElements.forEach(drumElement => {
+            // Toggle the visual styles
+            drumElement.classList.add('playing');
+            setTimeout(() => drumElement.classList.remove('playing'), 50)
+        })
     }
 
     if (audioElement) {
