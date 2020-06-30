9e9b239b715da9d378bc879f29b1a5c08d9c3eb1
Kyle Pollard <kylejrp@gmail.com>
2020-06-30 09:26:25 -0700

    Added randomization

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 const root = document.documentElement;
 
-const addAndFireInputListener = (selector, variable) => {
+const setupElementForCssVariable = (selector, variable) => {
     const element = document.querySelector(selector);
+
+    // Randomize the starting values and offsets
+    if (element.type === "range") {
+        randomizeInitialValues(element)
+    }
+
+    // Add the event listener for when the input changes
     const handleElementInput = () => handleInput(element, variable);
     element.addEventListener('input', handleElementInput);
+
+    // Handle the initial values set on the element
     handleElementInput();
+}
+
+const randomizeInitialValues = (element) => {
+    const { step, min, max } = element;
+    const { offset } = element.dataset;
+
+    const numberOfDiscreteValues = ((max - min) / step) + 1;
+
+    const random = getRandomInt(0, numberOfDiscreteValues);
+    element.value = parseInt(min) + (random * step);
+
+
+    if (offset) {
+        const randomOffset = getRandomInt(0, numberOfDiscreteValues);
+        element.dataset.offset = parseInt(min) + (randomOffset * step)
+    }
+
+}
+
+// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
 const handleInput = (element, variable) => {
@@ -27,7 +60,7 @@ const handleInput = (element, variable) => {
     }
 }
 
-addAndFireInputListener('input#rotation', '--rotation');
-addAndFireInputListener('input#blur', '--blur');
-addAndFireInputListener('input#base', '--base');
+setupElementForCssVariable('input#rotation', '--rotation');
+setupElementForCssVariable('input#blur', '--blur');
+setupElementForCssVariable('input#base', '--base');
 
