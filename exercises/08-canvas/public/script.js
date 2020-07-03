fd7de34bce52873398b5e189c309ccd20c8b16c6
Kyle Pollard <kylejrp@gmail.com>
2020-07-03 10:47:31 -0700

    Hue and line width

--- exercises/08-canvas/public/script.js
+++ exercises/08-canvas/public/script.js
@@ -3,22 +3,35 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-context.strokeStyle = '#BADA55'
 context.lineJoin = 'round';
 context.lineCap = 'round';
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let degrees = 0.0;
+const radians = () => degrees * Math.PI / 180.0;
 
 function draw(e) {
     if (!isDrawing) { return; }
 
-    context.lineWidth = 100;
+    const lineWidthSpeed = 3.0
+    const lineWidthMagnitude = ((Math.cos(radians() * lineWidthSpeed) + 1) / 2);
+    const lineWidth = (lineWidthMagnitude * 90) + 10;
+
+    const hueSpeed = 1.0;
+    const color = `hsl(${degrees * hueSpeed}, 100%, 50%)`;
+
+    context.lineWidth = lineWidth;
+    context.strokeStyle = color;
     context.beginPath();
     context.moveTo(lastX, lastY);
     context.lineTo(e.offsetX, e.offsetY);
     context.stroke();
+
+    console.log(degrees, lineWidthMagnitude, (lineWidthMagnitude * 90) + 10)
+
+    degrees = (degrees + 1.0) % 360.0;
 
     [lastX, lastY] = [e.offsetX, e.offsetY];
 }
